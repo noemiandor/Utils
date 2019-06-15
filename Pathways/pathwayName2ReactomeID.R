eada4eac9231461af47d4e0791804029476766fe
Noemi Andor <noemi@ONC-MACAIR-NA-2.local>
2019-06-15 09:13:06 -0700

    verbose option for pathway categories

--- Pathways/pathwayName2ReactomeID.R
+++ Pathways/pathwayName2ReactomeID.R
@@ -1,4 +1,4 @@
-pathwayName2ReactomeID<-function(targets, pnames){
+pathwayName2ReactomeID<-function(targets, pnames, verbose=T){
   library("stringdist"); 
   ids=rep(NA, length(targets));   names(ids)=tolower(targets)
   
@@ -14,10 +14,14 @@ pathwayName2ReactomeID<-function(targets, pnames){
     
     ids[tolower(x)]=pnames$`Event Name`[apply(tmp,2,which.min)]
     print(paste("Found approximate matches for",sum(d<=10),"targets:"))
-    print(ids[tolower(x)[d<=10]])
+    if(verbose){
+      print(ids[tolower(x)[d<=10]])
+    }
     print(paste("Found no good matches for",sum(d>10),"targets:"))
-    print(ids[tolower(x)[d>10]])
-
+    if(verbose){
+      print(ids[tolower(x)[d>10]])
+    }
+    
     ids[tolower(x)]=pnames$ReactomeStableidentifier[apply(tmp,2,which.min)]
     ids[tolower(x)[d>10]]=NA
     
