07c0270b9204630661b648cc9524c93d96b799b8
Noemi <noemi.andor@gmail.com>
2019-09-23 11:12:13 -0400

    Update getGenesInvolvedIn.R
    multiple pathways as input

--- getGenesInvolvedIn.R
+++ getGenesInvolvedIn.R
@@ -1,23 +1,29 @@
-getGenesInvolvedIn<-function(pathway, dbs="ReactomePA"){
-  goi=c()
+getGenesInvolvedIn<-function(pathways, dbs="ReactomePA"){
   if("ReactomePA" %in% dbs){
+    allGoi=list()
     library(ReactomePA)
     library(igraph)
-    p <- pathways("hsapiens", 'reactome')[[pathway]]
-    g <- try(pathwayGraph(p),silent=T)
-    if(class(g)!="try-error"){
-      gg <- igraph.from.graphNEL(g)
-      gg <- as.undirected(gg)
-      V(gg)$name <- sub("[^:]+:", "", V(gg)$name)
-      goi=c(goi,V(gg)$name)
+    allP <- pathways("hsapiens", 'reactome')
+    for(pathway in pathways){
+      goi=c()
+      p = allP[[pathway]]
+      g <- try(pathwayGraph(p),silent=T)
+      if(class(g)!="try-error"){
+        gg <- igraph.from.graphNEL(g)
+        gg <- as.undirected(gg)
+        V(gg)$name <- sub("[^:]+:", "", V(gg)$name)
+        goi=c(goi,V(gg)$name)
+      }
+      allGoi[[pathway]] = goi;
     }
   }
   if("INPATH" %in% dbs){
+    allGoi=c()
     mydb = dbConnect(MySQL(), user='noemi', password='lala', dbname='INPATH')
     genes<-dbGetQuery(mydb,paste('select gene_names from pathway_genes where pathway_name=\'',pathway,'\'',sep=""))$gene_names
     .disconnectAll()
-    goi=c(goi,genes)
+    allGoi=c(allGoi,genes)
+    allGoi=gsub("^SYMBOL:","",allGoi, fixed = F)
   }
-  goi=gsub("^SYMBOL:","",goi, fixed = F)
-  return(goi)
+  return(allGoi)
 }
